fee70b81617e9682e473bf2f8e0fef53f7fedf85
Alex Ayala <96927606+Alex2058v@users.noreply.github.com>
2022-10-13 22:57:06 -0600

    Add files via upload

---
+++ Avance de vista 2/js/Notas.js
@@ -0,0 +1,6 @@
+function mostrarTablaDatos(){
+    document.getElementById('tablaParaAnadir').style.display = "block";
+}
+function ocutarTabla(){
+    document.getElementById('tablaParaAnadir').style.display = "none";
+}
